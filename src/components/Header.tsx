34fdc0407776a0148d74f3fd210ac9d30bd45f6a
Jadson <jadsonmath874@gmail.com>
2021-03-27 21:34:31 -0400

    adiçao do menu deslizante

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Header.module.css';
+
+export function Header() {
+    return (
+        <div className={styles.headerContainer}>
+            <h1>Header</h1>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,30 @@
 import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { MainCards } from "../components/MainCards";
 import { Menu } from "../components/Menu";
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <header>
+        <Header />
+      </header>
+
       <section>
-        <div className={styles.headerContainer}>
-          {/* colocar o header da pagina aqui */}
-
-        </div>
-
         <div>
           <Menu />
         </div>
 
-        <div className={styles.cardContainer}>
+        <main>
+          <MainCards />
+        </main>
 
-        </div>
-
-        <footer>
-          <Footer />
-        </footer>
       </section>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
